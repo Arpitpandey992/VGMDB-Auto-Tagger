212f557b1b5c5d494510f99dd25ed9c5dbe3b65f
Arpitpandey992 <arpitpandey992@gmail.com>
2022-12-30 02:25:37 +0530

    Made more user friendly

--- FLAC_auto_tag.py
+++ FLAC_auto_tag.py
@@ -9,8 +9,9 @@ import shutil
 
 
 # languages to be probed from VGMDB in the given order of priority
-languages = ['en', 'English', 'ja-latn', 'Romaji', 'ja', 'Japanese']
-BACKUPFOLDER = 'D:\\backups'
+languages = ['ja-latn', 'Romaji', 'en', 'English',
+             'English (Apple Music)', 'ja', 'Japanese']
+BACKUPFOLDER = '/run/media/arpit/DATA/backups'
 APICALLRETRIES = 5
 
 ################## Helper Functions ############################
@@ -48,7 +49,7 @@ def getAlbumDetails(albumID):
 
 
 def searchAlbum(albumName):
-    return Request(f'https://vgmdb.info/search/{albumName}')
+    return Request(f'https://vgmdb.info/search?q={albumName}')
 
 
 def getBest(obj, orig='NA'):
@@ -58,7 +59,40 @@ def getBest(obj, orig='NA'):
     return orig
 
 
-def getTrackData(data):
+def hasCoverOfType(audio, typ):
+    for picture in audio.pictures:
+        if picture.type == typ:
+            return True
+    return False
+
+
+def getCount(discNumber):
+    # get the count of tracks -> checks if the input is something like 4/20 -> truncates to 4
+    if '/' in discNumber:
+        discNumber = discNumber.split('/')[0]
+    if ':' in discNumber:
+        discNumber = discNumber.split('/')[0]
+    return int(discNumber)
+
+
+def getOneFlacFile(folderPath):
+    for root, dirs, files in os.walk(folderPath):
+        for file in files:
+            if file.endswith('.flac'):
+                return os.path.join(root, file)
+    return None
+
+
+def yesNoUserInput():
+    print('Continue? (y/n) : ', end='')
+    resp = input()
+    if resp not in 'yY':
+        return False
+    return True
+    print('\n', end='')
+
+
+def getAlbumTrackData(data):
     trackData = {}
     discNumber = 1
     for disc in data['discs']:
@@ -71,27 +105,69 @@ def getTrackData(data):
     return trackData
 
 
-def hasCoverOfType(audio, typ):
-    for picture in audio.pictures:
-        if picture.type == typ:
-            return True
-    return False
+def getFolderTrackData(folderPath):
+    folderTrackData = {}
+    for root, dirs, files in os.walk(folderPath):
+        for file in files:
+            if not file.endswith('.flac'):
+                continue
 
-# get the count of tracks -> checks if the input is something like 4/20 -> truncates to 4
+            filePath = os.path.join(root, file)
+            audio = FLAC(filePath)
+            discNumber = 1
+            trackNumber = 1
+            try:
+                discNumber = getCount(audio['discnumber'][0])
+            except Exception as e:
+                print(
+                    f'Disc Number not Present in file : {file}, Taking Default Value = 01')
+            try:
+                trackNumber = getCount(audio['tracknumber'][0])
+            except Exception as e:
+                print(
+                    f'TrackNumber not Present in file : {file}, Skipped!')
+                continue
+
+            if discNumber not in folderTrackData:
+                folderTrackData[discNumber] = {}
+            if trackNumber in folderTrackData[discNumber]:
+                print(
+                    f'disc {discNumber}, Track {trackNumber} - {folderTrackData[discNumber][trackNumber]} Already Present')
+                print(f'cannot add {file}')
+                continue
+
+            folderTrackData[discNumber][trackNumber] = filePath
+    print('Done getting TrackData')
+    return folderTrackData
 
 
-def getCount(discNo):
-    if '/' in discNo:
-        discNo = discNo.split('/')[0]
-    if ':' in discNo:
-        discNo = discNo.split('/')[0]
-    return int(discNo)
+def doTracksAlign(albumTrackData, folderTrackData):
+    flag = True
+    if len(albumTrackData) != len(folderTrackData):
+        flag = False
+    for discNumber, tracks in albumTrackData.items():
+        if(discNumber not in folderTrackData or len(tracks) != len(folderTrackData[discNumber])):
+            flag = False
+
+    for discNumber, tracks in albumTrackData.items():
+        for trackNumber, trackTitle in tracks.items():
+            if discNumber not in folderTrackData or trackNumber not in folderTrackData[discNumber]:
+                print('{:<50s} {:<10s} {:<50s}'.format(
+                    f'{discNumber}/{trackNumber}. {trackTitle}',
+                    '->',
+                    '**NOT AVAILABLE**'))
+            else:
+                print('{:<50s} {:<10s} {:<50s}'.format(
+                    f'{discNumber}/{trackNumber}. {trackTitle}',
+                    '->',
+                    os.path.basename(folderTrackData[discNumber][trackNumber])))
+    print('\n', end='')
+    return flag
+
 
 ########################## Helper Functions ############################
-
-
-# flags
-PICS = True
+    # flags
+PICS = False
 ARRANGERS = False
 COMPOSERS = False
 ORGANIZATIONS = True
@@ -105,21 +181,6 @@ CONFIRM = True
 BACKUP = True
 
 
-def backupFolder(inputFolder):
-    try:
-        destinationFolder = BACKUPFOLDER
-        print(f'Backing Up {inputFolder}...')
-        backupFolder = os.path.join(
-            destinationFolder, os.path.basename(inputFolder))
-        shutil.copytree(inputFolder, backupFolder, dst_dir_exists_ok=True)
-        print(f'Successfully copied {inputFolder} to {backupFolder}')
-
-    except Exception as e:
-        print(f'{inputFolder} Backup Failed, aborting Program')
-        print(e)
-        exit(0)
-
-
 def tag_files(folderPath, albumID):
     try:
         data = getAlbumDetails(albumID)
@@ -130,18 +191,60 @@ def tag_files(folderPath, albumID):
         print('Failed, Please Try Again.')
         print(e)
         return False
+    albumLink = data["vgmdb_link"]
+    while albumLink and albumLink[-1] != '?':
+        albumLink = albumLink[:-1]
+    albumLink = albumLink[:-1]
 
     if CONFIRM:
-        print(f'Album - {getBest(data["names"])} OK? (y/n)')
-        print(f'Link - {data["vgmdb_link"]}')
-        resp = input()
-        if resp not in 'yY':
+        print(f'Link - {albumLink}')
+        print(f'Album - {getBest(data["names"])}')
+        if not yesNoUserInput():
+            print('\n', end='')
             return False
 
-    if BACKUP:
-        backupFolder(folderPath)
+    print('\n', end='')
+    print('\n', end='')
+    albumTrackData = getAlbumTrackData(data)
+    folderTrackData = getFolderTrackData(folderPath)
+    print('\n', end='')
 
-    trackData = getTrackData(data)
+    if not doTracksAlign(albumTrackData, folderTrackData):
+        print('The tracks are not fully fitting the album data received from VGMDB!')
+        if not yesNoUserInput():
+            print('\n', end='')
+            return False
+    else:
+        print('Tracks are perfectly aligning with the album data!')
+
+    print('\n', end='')
+    print('\n', end='')
+    if BACKUP:
+        try:
+            destinationFolder = BACKUPFOLDER
+            print(f'Backing Up {folderPath}...')
+            print('\n', end='')
+            backupFolder = os.path.join(
+                destinationFolder, os.path.basename(folderPath))
+            shutil.copytree(folderPath, backupFolder, dirs_exist_ok=False)
+            print(f'Successfully copied {folderPath} to {backupFolder}')
+
+        except Exception as e:
+            print("Backup Couldn't Be Completed :(")
+            print(e)
+            if not yesNoUserInput():
+                return False
+    print('\n', end='')
+    print('\n', end='')
+
+    totalTracks = 0
+    for disc in albumTrackData:
+        totalTracks += len(albumTrackData[disc])
+    totalDisks = len(albumTrackData)
+    tracksUpperBound = int(math.ceil(math.log10(totalTracks+1)))
+    disksUpperBound = int(math.ceil(math.log10(totalDisks+1)))
+    albumName = getBest(data['names'])
+
     if(PICS and 'picture_full' in data):
         response = requests.get(data['picture_full'])
         image_data = response.content
@@ -150,110 +253,67 @@ def tag_files(folderPath, albumID):
         picture.type = 3
         picture.mime = 'image/jpeg'
 
-    for root, dirs, files in os.walk(folderPath):
-        for file in files:
-            if not file.endswith('.flac'):
+    for discNumber, tracks in albumTrackData.items():
+        for trackNumber, trackTitle in tracks.items():
+            if discNumber not in folderTrackData or trackNumber not in folderTrackData[discNumber]:
                 continue
-            try:
-                filePath = os.path.join(root, file)
-                audio = FLAC(filePath)
-                # Tagging Album specific Details
-                audio['album'] = getBest(data['names'])
 
-                if DATE and 'release_date' in data:
-                    audio['date'] = standardize_date(data['release_date'])
+            filePath = folderTrackData[discNumber][trackNumber]
+            fileName = os.path.basename(filePath)
+            audio = FLAC(filePath)
 
-                if CATALOG and 'catalog' in data:
-                    audio['catalog'] = data['catalog']
+            # Tagging Album specific Details
 
-                if BARCODE and 'barcode' in data:
-                    audio['barcode'] = data['barcode']
+            audio['album'] = albumName
+            audio['tracktotal'] = str(totalTracks)
+            audio['disctotal'] = str(totalDisks)
+            audio['comment'] = f'Find the tracklist at {albumLink}'
 
-                if(PICS and 'picture_full' in data and not hasCoverOfType(audio, 3)):
-                    audio.add_picture(picture)
+            if DATE and 'release_date' in data:
+                audio['date'] = standardize_date(data['release_date'])
 
-                if 'organizations' in data and ORGANIZATIONS:
-                    for org in data['organizations']:
-                        audio[org['role']] = getBest(org['names'])
+            if CATALOG and 'catalog' in data:
+                audio['catalog'] = data['catalog']
 
-                def addMultiValues(tag, tagInFile, flag=True):
-                    if tag in data and flag:
-                        temp = []
-                        for val in data[tag]:
-                            temp.append(getBest(val['names']))
-                        audio[tagInFile] = temp
+            if BARCODE and 'barcode' in data:
+                audio['barcode'] = data['barcode']
 
-                addMultiValues('lyricists', 'lyricist', LYRICISTS)
-                addMultiValues('performers', 'performer', PERFORMERS)
-                addMultiValues('arrangers', 'arranger', ARRANGERS)
-                addMultiValues('composers', 'composer', COMPOSERS)
+            if(PICS and 'picture_full' in data and not hasCoverOfType(audio, 3)):
+                audio.add_picture(picture)
 
-                # if 'lyricists' in data and LYRICISTS:
-                #     temp = []
-                #     for lyr in data['lyricists']:
-                #         temp.append(getBest(lyr['names']))
-                #     audio['lyricist'] = temp
+            if ORGANIZATIONS and 'organizations' in data:
+                for org in data['organizations']:
+                    audio[org['role']] = getBest(org['names'])
 
-                # if 'performers' in data and PERFORMERS:
-                #     temp = []
-                #     for per in data['performers']:
-                #         temp.append(getBest(per['names']))
-                #     audio['performer'] = temp
+            def addMultiValues(tag, tagInFile, flag=True):
+                if tag in data and flag:
+                    temp = []
+                    for val in data[tag]:
+                        temp.append(getBest(val['names']))
+                    audio[tagInFile] = temp
 
-                # if 'arrangers' in data and ARRANGERS:
-                #     temp = []
-                #     for arr in data['arrangers']:
-                #         temp.append(getBest(arr['names']))
-                #     audio['arranger'] = temp
+            addMultiValues('lyricists', 'lyricist', LYRICISTS)
+            addMultiValues('performers', 'performer', PERFORMERS)
+            addMultiValues('arrangers', 'arranger', ARRANGERS)
+            addMultiValues('composers', 'composer', COMPOSERS)
 
-                # if 'composers' in data and COMPOSERS:
-                #     temp = []
-                #     for comp in data['composers']:
-                #         temp.append(getBest(comp['names']))
-                #     audio['composer'] = temp
+            # Tagging track specific details
 
-                # Tagging track specific details
-                discNo = 1
-                trackNo = 1
-                try:
-                    discNo = getCount(audio['discnumber'][0])
-                except Exception as e:
-                    print(
-                        f'DiscNumber not Present in file {file}, Taking Default Value = 01')
-                try:
-                    trackNo = getCount(audio['tracknumber'][0])
-                except Exception as e:
-                    # exception not allowed in TrackNumber
-                    print(f'TrackNumber not Present in file {file}, Skipped!')
-                    continue
+            audio['title'] = trackTitle
+            audio['discnumber'] = str(discNumber).zfill(disksUpperBound)
+            audio['tracknumber'] = str(trackNumber).zfill(tracksUpperBound)
 
-                trackTitle = trackData[discNo][trackNo]
-                totalTracks = len(trackData[discNo])
-                totalDisks = len(trackData)
-                tracksUpperBound = int(math.ceil(math.log10(totalTracks+1)))
-                disksUpperBound = int(math.ceil(math.log10(totalDisks+1)))
+            print('{:<10s} {:<50s} {:10s} {:<50s}'.format(
+                'File Tagged : ',
+                fileName,
+                '->',
+                f'{discNumber}/{trackNumber}. {trackTitle}'
+            ))
+            audio.save()
 
-                audio['tracknumber'] = str(trackNo).zfill(tracksUpperBound)
-                audio['tracktotal'] = str(totalTracks)
-                audio['discnumber'] = str(discNo).zfill(disksUpperBound)
-                audio['disctotal'] = str(totalDisks)
-                audio['title'] = trackTitle
-
-                print(
-                    f'--> Done with file : {file} -> {trackNo}. {trackTitle}')
-                audio.save()
-
-            except Exception as e:
-                print(e)
+    print('\n', end='')
+    print('\n', end='')
     return True
-
-
-def getOneFlacFile(folderPath):
-    for root, dirs, files in os.walk(folderPath):
-        for file in files:
-            if file.endswith('.flac'):
-                return os.path.join(root, file)
-    return None
 
 
 def findAndTagAlbum(folderPath):
@@ -261,13 +321,16 @@ def findAndTagAlbum(folderPath):
     filePath = getOneFlacFile(folderPath)
     if filePath is None:
         print('No Flac File Present in the directory')
+        print('\n', end='')
+        print('\n', end='')
         return None
 
     flac = FLAC(filePath)
     if 'album' not in flac or not flac['album']:
         return None
     albumName = flac.get("album")[0]
-
+    print(f'Searching for album : {albumName}')
+    print('\n', end='')
     data = searchAlbum(albumName)
     if data is None or not data['results']['albums']:
         return None
@@ -278,17 +341,28 @@ def findAndTagAlbum(folderPath):
     return False
 
 
-folderPath = r"D:\OSTs\Visual Novels\07th Expansion\02 - Umineko\02 - Visual Novel (Console)\[2012.01.25] [YZPB-5001] Inanna no Mita Yume - Zwei"
-
+folderPath = "/run/media/arpit/DATA/OSTs/Anime/Fairy Tail/FAIRY TAIL ORIGINAL SOUND COLLECTION"
 parser = argparse.ArgumentParser(description='Check and Fix FLAC Files')
 parser.add_argument('folderPath', nargs='?', help='Flac directory path')
-
+parser.add_argument('--ID', '-i', type=str, default=None,
+                    help='Provide Album ID')
 args = parser.parse_args()
-if args.folderPath:
-    folderPath = args.folderPath
+
 
 print('Started...')
-if findAndTagAlbum(folderPath):
-    print('Done Tagging')
+print('\n', end='')
+print('\n', end='')
+if folderPath[-1] == '/':
+    folderPath = folderPath[:-1]
+if args.folderPath:
+    folderPath = args.folderPath
+if args.ID is not None:
+    if tag_files(folderPath, args.ID):
+        print('Done Tagging')
+    else:
+        print(f"Couldn't Tag the Album at {folderPath}")
 else:
-    print(f"Couldn't Tag the Album at {folderPath}")
+    if findAndTagAlbum(folderPath):
+        print('Done Tagging')
+    else:
+        print(f"Couldn't Tag the Album at {folderPath}")
