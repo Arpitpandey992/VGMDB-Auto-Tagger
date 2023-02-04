7659d1a28d8c953df5bc14b92b19659a5dfcc2e7
Arpitpandey992 <arpitpandey992@gmail.com>
2023-02-04 10:30:39 +0530

    feat: add venv support in autoSearchAlbumScript

--- Scripts/autoSearchAlbumScript.sh
+++ Scripts/autoSearchAlbumScript.sh
@@ -3,6 +3,7 @@
 CWD=$(pwd)
 for d in */ ; do
     cd "$d"
+    source /home/arpit/Programming/Python/VGMDB-Auto-Tagger/venv/bin/activate
     python ~/Programming/Python/VGMDB-Auto-Tagger/albumTagger.py "$(pwd)" --yes
     cd "$CWD"
 done
