43346079490e5c41c001e4ebc05a548345f54090
Arpitpandey992 <arpitpandey992@gmail.com>
2023-04-04 19:04:24 +0530

    fix: scripts, add another cover

--- Scripts/autoSearchAlbumScriptMultiTreaded.sh
+++ Scripts/autoSearchAlbumScriptMultiTreaded.sh
@@ -6,7 +6,7 @@ CWD=$(pwd)
 function run_command {
     dir="$1"
     abs_dir=$(realpath "$dir") # Convert relative path to absolute path
-    konsole --workdir "$abs_dir" -e bash -c "source /home/arpit/Programming/Python/VGMDB-Auto-Tagger/venv/bin/activate && python ~/Programming/Python/VGMDB-Auto-Tagger/albumTagger.py \"$abs_dir\" --yes"
+    konsole --workdir "$abs_dir" -e bash -c "source /home/arpit/Programming/Python/VGMDB-Auto-Tagger/.venv/bin/activate && python ~/Programming/Python/VGMDB-Auto-Tagger/albumTagger.py \"$abs_dir\" --yes"
     wait
     sleep 20
     kill $(jobs -p) # Close the terminal window
