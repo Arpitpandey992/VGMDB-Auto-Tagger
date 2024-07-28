f3b0d5b246b73c0aa43ef2b3bbe1dc6c3d2cb723
Arpitpandey992 <arpitpandey992@gmail.com>
2024-07-28 17:10:41 +0530

    chore: remove obsolete files

--- Scripts/autoOrganize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CWD=$(pwd)
-for d in */ ; do
-    cd "$d"
-    source /home/arpit/Programming/Python/VGMDB-Auto-Tagger/.venv/bin/activate
-    python ~/Programming/Python/VGMDB-Auto-Tagger/organize.py "$(pwd)"
-    cd "$CWD"
-done
